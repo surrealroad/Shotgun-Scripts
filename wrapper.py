cf69850b640c63c98d559fceaa7290d73bf926a9
Jack James <jack@surrealroad.com>
2016-11-03 17:08:02 +0000

    set path to allow importing from Scripts folder

--- wrapper.py
+++ wrapper.py
@@ -12,17 +12,18 @@ Created by Jack James on 2015-07-10.
 
 """
 
-import sys, imp
+import sys, imp, os
 
 if __name__ == '__main__':
     args = ()
     if(len(sys.argv)>3):
         args = tuple(sys.argv[3:])
     path = sys.argv[1]
+    sys.path.append(os.path.dirname(path))
     module = imp.load_source('script', path)
     func = getattr(module, sys.argv[2], None)
     if func is not None:
         func(*args)
     else:
-        print("ERROR: Module or function missing")
+        raise ImportError("Module or function missing")
 
